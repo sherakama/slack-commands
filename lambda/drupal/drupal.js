ef85d55dcedcf12fc345a144be652445bd2c27a6
Sherakama <sherakama@gmail.com>
2021-04-01 11:25:48 -0700

    Update drupal.js

--- lambda/drupal/drupal.js
+++ lambda/drupal/drupal.js
@@ -1,19 +1,5 @@
 /**
- * MEGAPROFILE API MICROSERVICE
- * 
- * This microservice fetches information from the megaprofile api. There is only
- * one endpoint with one parameter. You can fetch information from the API by
- * the person's ENCODEDSUID id. To connect to the API endpoint you have to be
- * authenticated via oauth and pass a bearer token. On each request to the
- * microservice endpoint we fetch a new bearer token and use that in the request
- * for profile data.
- * 
- * This service is only responsible for handling the transaction to/from the API
- * and does not modify or shape the results in any way.
- * 
- * Basic auth is enabled on the profile endpoint to protect abuse of this 
- * service but clients should strive to only access this endpoint from the 
- * "Server Side".
+ * APRIL FOOLS
  */
 
 // Variables.
